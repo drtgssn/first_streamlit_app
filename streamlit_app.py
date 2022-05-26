24d0dbd23ecd9788f25c4c261e3d40034a6ca2bb
Duarte Gassin <106310631+drtgssn@users.noreply.github.com>
2022-05-26 12:56:48 +0100

    Update streamlit_app.py
    Add comments and a picklist for users to choose fruits for smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-import pandas 
+import pandas
+
+# Create menu with title, headers and text
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -9,6 +11,13 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+# import fruit data into a table from csv using pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# create picklist for users to select the fruits for the smoothie
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
+
+# display table on the page
 streamlit.dataframe(my_fruit_list)
 
