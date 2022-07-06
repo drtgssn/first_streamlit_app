6d015d91f00b5b9f3fc3297e1b2ed94d38ef270d
Duarte Gassin <106310631+drtgssn@users.noreply.github.com>
2022-07-06 11:16:24 +0100

    Update streamlit_app.py
    Changed fruit list index to fruit column

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import fruit list from Amazon S3 Bucket
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #add fruit picklist for users to choose fruits
 streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
