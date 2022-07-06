fde100275cd5e53f65e54055283a75f69a9e88bc
Duarte Gassin <106310631+drtgssn@users.noreply.github.com>
2022-07-06 11:19:42 +0100

    Update streamlit_app.py
    Added default picklist items

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #add fruit picklist for users to choose fruits
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 #display fruit list
 streamlit.dataframe(my_fruit_list)
