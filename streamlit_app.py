df5febd6e7fbbddcf1dc8ddcff2b0b407c62989a
Duarte Gassin <106310631+drtgssn@users.noreply.github.com>
2022-07-06 11:01:39 +0100

    Update streamlit_app.py
    Edited menu items and added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit
 
 ## Create menu with title, headers and text
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 #--------------- OLD VERSION
 #import streamlit
