eb4cfe81eb0dccc6a788643e3071920a3078199a
Duarte Gassin <106310631+drtgssn@users.noreply.github.com>
2022-07-24 13:52:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # snowflake connector
-$ pip install pyarrow
+pip install pyarrow
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
